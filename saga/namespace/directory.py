69a4e96987f11ec8b745e359b2f0fdaa8c8129cc
Alexander Grill <alexander.grill@tuwien.ac.at>
2013-10-31 14:34:14 +0100

    Added remove_self() method

--- saga/namespace/directory.py
+++ saga/namespace/directory.py
@@ -1,5 +1,5 @@
 
-__author__    = "Andre Merzky"
+__author__    = "Andre Merzky, Ole Weidner, Alexander Grill"
 __copyright__ = "Copyright 2012-2013, The SAGA Project"
 __license__   = "MIT"
 
@@ -409,7 +409,29 @@ class Directory (entry.Entry) :
         '''
         if tgt    :  return self._adaptor.remove (tgt, flags, ttype=ttype)
         else      :  return self._nsentry.remove (     flags, ttype=ttype)
-  
+
+    # --------------------------------------------------------------------------
+    #
+    @sus.takes   ('Directory',
+                  sus.optional (sus.one_of (SYNC, ASYNC, TASK)))
+    @sus.returns ((sus.nothing, st.Task))
+    def remove_self (self, flags=0, ttype=None) :
+        '''
+        :param flags:  Flags to use for the operation.
+
+        ttype:         saga.task.type enum
+        ret:           None / saga.Task
+        
+        Reove the entry.
+
+        The entry is removed, and this object instance is then invalid for
+        further operations.
+
+            # remove an entry
+            entry = saga.namespace.Directory("sftp://localhost/tmp/data/data.bin")
+            entry.remove ()
+        '''
+        return self._adaptor.remove_self (flags, ttype=ttype)   
     
     # --------------------------------------------------------------------------
     #
